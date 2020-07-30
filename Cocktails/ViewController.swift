2afd8f7e95b5e80d9b2a68254809507d7decf1a0
Guillaume Ramey <guillaume.ramey@gmail.com>
2020-07-30 16:32:38 +0200

    Initial Commit

---
+++ Cocktails/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cocktails
+//
+//  Created by Guillaume Ramey on 30/07/2020.
+//  Copyright © 2020 Guillaume Ramey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
